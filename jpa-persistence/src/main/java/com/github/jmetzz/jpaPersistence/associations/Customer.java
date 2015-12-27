db2ef8057210b681924e9912bd9dd2ea7fe73d00
Jean Metz <jmetzz@gmail.com>
2015-12-27 14:55:23 +0100

    WIP - JPA + association mapping;

---
+++ jpa-persistence/src/main/java/com/github/jmetzz/jpaPersistence/associations/Customer.java
@@ -0,0 +1,109 @@
+package com.github.jmetzz.jpaPersistence.associations;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Jean Metz.
+ */
+@Entity
+public class Customer {
+    String phoneNumber;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_fk")
+    private Address homeAddress;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+
+    /* Because age can be automatically calculated from the date of birth,
+     the age attribute does not need to be mapped and therefore can be transient.
+     As a result, the age attribute doesn’t need any AGE column to be mapped to.
+     */
+    @Transient
+    private Integer age;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+}
---
+++ jpa-persistence/src/main/java/com/github/jmetzz/jpaPersistence/associations/PhoneType.java
@@ -0,0 +1,8 @@
+package com.github.jmetzz.jpaPersistence.associations;
+
+/**
+ * Created by Jean Metz.
+ */
+public enum PhoneType {
+    HOME, WORK, MOBILE
+}
